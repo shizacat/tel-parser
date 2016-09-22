cc279963b2285743e9a349954bccac307bfa8490
mav <a.matveev@cherinfo.ru>
2016-09-22 13:39:00 +0300

    Вторая версия

---
+++ db-template.sql
@@ -0,0 +1,74 @@
+
+
+CREATE TABLE IF NOT EXISTS invoices (bn integer UNIQUE, an varchar(128), sd Date, ed Date, bd Date, pn varchar(256), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4), od varchar(64), PRIMARY KEY(bn, an) );
+
+
+bn - номер счета
+an - номер лицевого счета абонента
+sd - ed - период за который выставлен счет
+bd - от какого числа счет
+pn - Юридическое имя плательщика
+ha  - Израсходовано за период
+hua - К оплате
+hr  - Остаток
+od - Возможно!!! Баланс технологисческого ЛС
+
+
+-- Сводная таблица лицевых счетов
+CREATE TABLE IF NOT EXISTS rrp (bn integer, an varchar(128), cn varchar(128), n varchar(128), mr varchar(256), reg varchar(256), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4),  FOREIGN KEY (bn, an) REFERENCES invoices ON DELETE CASCADE, PRIMARY KEY(bn, an, cn) );
+bn
+an
+cn - номер контракта
+n - Юридическое лица
+mr - МР
+reg - регион
+ha  - Израсходовано за период
+hua - К оплате
+hr  - Остаток
+
+-- Лицевые счета к сводной таблице лицевых счетов
+CREATE TABLE IF NOT EXISTS pai (bn integer, an varchar(128), cn varchar(128), pa varchar(128), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4), FOREIGN KEY (bn, an, cn) REFERENCES rrp ON DELETE CASCADE, PRIMARY KEY(bn, an, cn, pa));
+bn
+an
+cn
+pa - номер лицевого счета
+ha  - Израсходовано за период
+hua - К оплате
+hr  - Остаток
+
+-- Расход ресурсов по номерам
+CREATE TABLE IF NOT EXISTS upss (bn integer, an varchar(128), pa varchar(128), m varchar(128), ssp_ha numeric(15,4), sso_ha numeric(15,4), sst_ha numeric(15,4), ssn_ha numeric(15,4), ssa_ha numeric(15,4), FOREIGN KEY (bn, an) REFERENCES invoices(bn, an) ON DELETE CASCADE, PRIMARY KEY(bn, an, pa, m) );
+bn
+an
+pa - номер лицевого счета
+m - номер (сетевой ресурс)
+ssp_a - Переодические услуги
+sso_a - Разовые услуги
+sst_a - Телефонные услуги
+ssn_a - НДС
+ssa_a - Итого
+
+
+-- Детализация номеров
+CREATE TABLE IF NOT EXISTS details (bn integer, an varchar(128), nr varchar(128), d varchar(128), n varchar(128), zp varchar(128), zv varchar(128), a varchar(128), du varchar(128), c varchar(128), dup varchar(128), f varchar(128), gmt varchar(128), s varchar(128), hd timestamp, hdu_s smallint, hc numeric(15,4), hn varchar(42), cdirection char(1), hnsyf varchar(128), FOREIGN KEY (bn, an) REFERENCES invoices(bn, an) ON DELETE CASCADE);
+
+nr - сетевой ресурс (номер телефона)
+d 
+n - номер из файла
+zp - зона направления
+zv - зона вызова
+a 
+du 
+c 
+dup 
+f 
+bd 
+cur 
+gmt 
+s - сервис (sms i, sms o, Телеф., ...)
+hd - дата вызова, без часового пояса
+hdu_s - продолжительность, в секундах
+hc - стоимость, NUMERIC(15,4)
+cdirection - направление вызова (i - входящие, o - исходящие)
+hn - нормализованный номер
+hnsyf - суфикс из номера. Примеры: Ya_na_svyazi, sms, Vam_Zvonili
--- tp-driver.py
+++ tp-driver.py
@@ -17,6 +17,8 @@ import psycopg2
 from datetime import datetime, timezone
 import sys, os, re
 
+from io import StringIO
+
 DB_HOST='localhost'
 DB_NAME='tel'
 DB_USER='test'
@@ -27,8 +29,7 @@ DB_TABLE='main'
 C_QUITE=False
 
 class bill:
-	add_cur = None
-	add_doing = False
+	add_buf = StringIO()
 
 	def __init__(self):
 		try:
@@ -51,7 +52,20 @@ class bill:
 		cur.close
 
 	def add_detail_commit(self):
-		pass
+		cur = self.conn.cursor()
+		self.add_buf.seek(0)
+		
+		try:
+			cur.copy_expert("copy details (bn, an, d, n, zp, zv, a, du, c, dup, f, gmt, s, hd, hdu_s, hc, nr, hn, cdirection, hnsyf) from STDIN" % (), self.add_buf)
+		except Exception as e:
+			self.conn.commit()
+			raise ValueError("Ошибка в запросе к бд: %s" % (e,))
+
+		self.conn.commit()
+		cur.close
+
+		self.add_buf.close()
+		self.add_buf = StringIO()
 
 
 	def add_detail(self, bn, an, nr, value):
@@ -107,6 +121,9 @@ class bill:
 			hnsyf = ''
 
 
+		# IO
+		# self.add_buf.write( "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (bn, an, value['d'], value['n'], value['zp'], value['zv'], value['a'], value['du'], value['c'], value['dup'], value['f'], value['gmt'], value['s'], hd, hdu_s, hc, nr, hn, cdirection, hnsyf ))
+
 		cur = self.conn.cursor()
 		
 		try:
@@ -124,112 +141,30 @@ class bill:
 		cur.close
 
 
+def setup():
+	lines = [
+	"DROP TABLE IF EXISTS invoices, rrp, pai, upss, details",
+	"CREATE TABLE IF NOT EXISTS invoices (bn integer UNIQUE, an varchar(128), sd Date, ed Date, bd Date, pn varchar(256), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4), od varchar(64), PRIMARY KEY(bn, an) )",
+	"CREATE TABLE IF NOT EXISTS rrp (bn integer, an varchar(128), cn varchar(128), n varchar(128), mr varchar(256), reg varchar(256), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4),  FOREIGN KEY (bn, an) REFERENCES invoices ON DELETE CASCADE, PRIMARY KEY(bn, an, cn) )",
+	"CREATE TABLE IF NOT EXISTS pai (bn integer, an varchar(128), cn varchar(128), pa varchar(128), ha numeric(15,4), hua numeric(15,4), hr numeric(15,4), FOREIGN KEY (bn, an, cn) REFERENCES rrp ON DELETE CASCADE, PRIMARY KEY(bn, an, cn, pa))",
+	"CREATE TABLE IF NOT EXISTS upss (bn integer, an varchar(128), pa varchar(128), m varchar(128), ssp_ha numeric(15,4), sso_ha numeric(15,4), sst_ha numeric(15,4), ssn_ha numeric(15,4), ssa_ha numeric(15,4), FOREIGN KEY (bn, an) REFERENCES invoices(bn, an) ON DELETE CASCADE, PRIMARY KEY(bn, an, pa, m) )",
+	"CREATE TABLE IF NOT EXISTS details (bn integer, an varchar(128), nr varchar(128), d varchar(128), n varchar(128), zp varchar(128), zv varchar(128), a varchar(128), du varchar(128), c varchar(128), dup varchar(128), f varchar(128), gmt varchar(128), s varchar(128), hd timestamp, hdu_s smallint, hc numeric(15,4), hn varchar(42), cdirection char(1), hnsyf varchar(128), FOREIGN KEY (bn, an) REFERENCES invoices(bn, an) ON DELETE CASCADE)",
+	]
 
-# Класс для добавления записей в базу
-class cnr:
-	# Колличество повторных ключей, и массив их
-	valEDK = 0
-	arrEDK = []
+	try:
+		conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DB_NAME, DB_USER, DB_HOST, DB_PASS))
 
-	def __init__(self, nr):
-		self.nr = nr
+		cur = conn.cursor()
 
-		try:
-			self.conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DB_NAME, DB_USER, DB_HOST, DB_PASS))
-		except Exception as e:
-			print("Не могу подключиться к базе. Ошибка: %s" % (e,))
-			raise ValueError
-
-		cur = self.conn.cursor()
-
-		try:
-			cur.execute("CREATE TABLE IF NOT EXISTS %s (nr varchar(128), d varchar(128), n varchar(128), zp varchar(128), zv varchar(128), a varchar(128), du varchar(128), c varchar(128), dup varchar(128), f varchar(128), gmt varchar(128), s varchar(128), hd timestamp, hdu_s smallint, hc numeric(15,4), hn varchar(42), cdirection char(1), hnsyf varchar(128), PRIMARY KEY(nr, hd, s))" %(DB_TABLE,) )
-			self.conn.commit()
-		except Exception as e:
-			raise ValueError(e)
-
-		cur.close
-
-	def __del__(self):
-		self.conn.close()
-
-	# Добавляем в базу - вызов(услугу) с его атрибутами
-	def add(self, value):
-		
-		attr_reqire = ['d', 'n', 'zv', 'zp', 'gmt', 'du', 'f', 'dup', 'c', 'a', 's' ] # 'cur', 'bd'
-
-		l = list(set(attr_reqire) - set(value.keys()))
-		if len(l) != 0:
-			raise ValueError("В списке отсутствуют обязательные поля: %s"%(l))
-
-		# Преобразуем дату к формату timestamp без часового пояса
-		hd = datetime.strptime( value['d'] + " " + value["gmt"].replace(':','') , "%d.%m.%Y %H:%M:%S %z")
-		hd = hd.astimezone(tz=timezone.utc).replace(tzinfo=None)
-
-		# Преобразуем длительность в секунды, которое храниться как целое
-		tmp_s = value["du"].split(':')
-		if len(tmp_s) == 2:
-			hdu_s = int(tmp_s[0])*60 + int(tmp_s[1])
-		else:
-			hdu_s = 0
-
-		# Преобразуем денежный тип к numeric
-		hc = value["c"].replace(',', '.')
-
-		# Нормализуем номер
-		hn = re.search('\d+', value["n"])
-		if hn:
-			hn = hn.group(0)
-			# Заменяем 8 в начале номера на 7
-			# if re.search('^8\d+', hn):
-			# 	hn = re.sub('^8', '+7', hn, 1);
-
-			# Заменяем 7 в начале номера на +7
-			if re.search('^7\d+', hn):
-				hn = re.sub('^7', '+7', hn, 1);
-		else:
-			hn = ''
-
-		# Определяем направление вызова
-		if re.match('^<--', value["n"]):
-			cdirection = 'i'
-		else:
-			cdirection = 'o'
-
-		# Суфикс поястения из номера сохраняем в отдельном поле
-		hnsyf = re.search('(?<=<--)[a-zA-Z0-9_-]+(?=:)', value["n"])
-		if hnsyf:
-			hnsyf = hnsyf.group(0)
-		else:
-			hnsyf = ''
-
-
-		cur = self.conn.cursor()
-		
-		try:
-			cur.execute("INSERT INTO %s (d, n, zp, zv, a, du, c, dup, f, gmt, s, hd, hdu_s, hc, nr, hn, cdirection, hnsyf) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s', '%s', '%s', '%s')" % (DB_TABLE, value['d'], value['n'], value['zp'], value['zv'], value['a'], value['du'], value['c'], value['dup'], value['f'], value['gmt'], value['s'], hd, hdu_s, hc, self.nr, hn, cdirection, hnsyf))
-		except psycopg2.IntegrityError:
-			self.valEDK += 1
-			self.arrEDK.append( str(hd) +", "+ str(value["s"]))
-			self.conn.rollback()
-		except Exception as e:
-			self.conn.commit()
-			raise ValueError("Ошибка в запросе к бд: %s" % (e,))
-
-		self.conn.commit()
-
-		cur.close
-
-	# Выполнения единочного запроса к бд
-	def execute(self, sql):
-		cur = self.conn.cursor()
-		try:
+		for sql in lines:
 			cur.execute(sql)
-		except Exception as e:
-			raise ValueError("Ошибка в запросе к бд: %s" % (e,)) 
 		
-		self.conn.commit()
+		conn.commit()
 		cur.close
+
+	except Exception as e:
+		print("Ошибка в запросе к бд: %s" % (e,))
+		sys.exit(-1)
 
 # В нужный формат sql
 def dtohd(dt):
@@ -251,80 +186,78 @@ def parse_xml(f_xml):
 
 	db = bill()
 
-	# Таблица Invoices
 	tmp_b = root.findall("b")[0].attrib	
-	tmp_c = root.findall("c")[0].attrib
-	tmp_urp = root.findall("urp")[0].attrib
 	bn = tmp_b["bn"]
 	an = tmp_b["an"]
-	db.execute("INSERT INTO invoices (bn, an, sd, ed, bd, pn, ha, hua, hr, od) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (bn, an, dtohd(tmp_b["sd"]), dtohd(tmp_b["ed"]), dtohd(tmp_b["bd"]), tmp_c["pn"], ctohc(tmp_urp["a"]), ctohc(tmp_urp["ua"]), ctohc(tmp_urp["r"]), tmp_urp["od"] ))
 
-	# Сводная таблица счетов
-	for rrp in root.findall("./urp/rrp"):
-		cn = rrp.attrib["cn"]
-		db.execute("INSERT INTO rrp (bn, an, cn, n, mr, reg, ha, hua, hr) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( bn, an, cn, rrp.attrib["n"], rrp.attrib["mr"], rrp.attrib["reg"], ctohc(rrp.attrib["a"]), ctohc(rrp.attrib["ua"]), ctohc(rrp.attrib["r"]) ))
-		
-		for pai in rrp.findall("pai"):
-			db.execute("INSERT INTO pai (bn, an, cn, pa, ha, hua, hr) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( bn, an, cn, pai.attrib["pa"], ctohc(pai.attrib["a"]), ctohc(pai.attrib["ua"]), ctohc(pai.attrib["r"]) ))
+	try:
 
-	# Расход ресурсов по номерам
-	for upss in root.findall("./utp/upss"):
-		pa = upss.attrib["pa"]
+		# Таблица Invoices
+		tmp_c = root.findall("c")[0].attrib
+		tmp_urp = root.findall("urp")[0].attrib
 
-		for ps in upss.findall("ps"):
-			m = ps.attrib["m"]		# номер
-			tp = ps.attrib["tp"]	# Тарифный план
-			ssn_ha = ps.attrib["t"]		# НДС
-			ssa_ha = ps.attrib["a"]		# Итого
-			ssp_ha = '0'
-			sso_ha = '0'
-			sst_ha = '0'
+		db.execute("INSERT INTO invoices (bn, an, sd, ed, bd, pn, ha, hua, hr, od) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (bn, an, dtohd(tmp_b["sd"]), dtohd(tmp_b["ed"]), dtohd(tmp_b["bd"]), tmp_c["pn"], ctohc(tmp_urp["a"]), ctohc(tmp_urp["ua"]), ctohc(tmp_urp["r"]), tmp_urp["od"] ))
 
-			for sc in ps.findall("sc"):
-				if sc.attrib["s"].lower() == "Периодические услуги".lower():
-					ssp_ha = sc.attrib["a"]
-					continue
-				if sc.attrib["s"].lower() == "Разовые услуги".lower():
-					sso_ha = sc.attrib["a"]
-					continue
-				if sc.attrib["s"].lower() == "Телефонные услуги".lower():
-					sst_ha = sc.attrib["a"]
-					continue
+		# Сводная таблица счетов
+		for rrp in root.findall("./urp/rrp"):
+			cn = rrp.attrib["cn"]
+			db.execute("INSERT INTO rrp (bn, an, cn, n, mr, reg, ha, hua, hr) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( bn, an, cn, rrp.attrib["n"], rrp.attrib["mr"], rrp.attrib["reg"], ctohc(rrp.attrib["a"]), ctohc(rrp.attrib["ua"]), ctohc(rrp.attrib["r"]) ))
+			
+			for pai in rrp.findall("pai"):
+				db.execute("INSERT INTO pai (bn, an, cn, pa, ha, hua, hr) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % ( bn, an, cn, pai.attrib["pa"], ctohc(pai.attrib["a"]), ctohc(pai.attrib["ua"]), ctohc(pai.attrib["r"]) ))
 
-			db.execute("INSERT INTO upss (bn, an, pa, m, ssp_ha, sso_ha, sst_ha, ssn_ha, ssa_ha) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (bn, an, pa, m, ctohc(ssp_ha), ctohc(sso_ha), ctohc(sst_ha), ctohc(ssn_ha), ctohc(ssa_ha) ))
+		# Расход ресурсов по номерам
+		for upss in root.findall("./utp/upss"):
+			pa = upss.attrib["pa"]
 
-	# Детализация звонков по номерам
-	for ds in root.findall("./ds"):
-		nr = ds.attrib["n"] 		# Сетевой ресурс (номер)
-		
-		for i in ds.findall('i'):
-			db.add_detail(bn, an, nr, i.attrib)
+			for ps in upss.findall("ps"):
+				m = ps.attrib["m"]		# номер
+				tp = ps.attrib["tp"]	# Тарифный план
+				ssn_ha = ps.attrib["t"]		# НДС
+				ssa_ha = ps.attrib["a"]		# Итого
+				ssp_ha = '0'
+				sso_ha = '0'
+				sst_ha = '0'
 
-		if not C_QUITE:
-	 		print("[I] Завершена обработка: %s. " % (nr))
+				for sc in ps.findall("sc"):
+					if sc.attrib["s"].lower() == "Периодические услуги".lower():
+						ssp_ha = sc.attrib["a"]
+						continue
+					if sc.attrib["s"].lower() == "Разовые услуги".lower():
+						sso_ha = sc.attrib["a"]
+						continue
+					if sc.attrib["s"].lower() == "Телефонные услуги".lower():
+						sst_ha = sc.attrib["a"]
+						continue
 
-	# for ds in root.findall("./ds"):
-	# 	n = ds.attrib["n"]
-		
-	# 	try:
-	# 		db = cnr( n)
-	# 	except ValueError as e:
-	# 		print("[!]", "[%s]"%(n,), e)
-	# 		continue
-		
-	# 	for i in ds.findall('i'):
-	# 		try:
-	# 			db.add(i.attrib)
-	# 		except ValueError as e:
-	# 			print("[!]", "[%s]"%(n,), e)
+				db.execute("INSERT INTO upss (bn, an, pa, m, ssp_ha, sso_ha, sst_ha, ssn_ha, ssa_ha) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (bn, an, pa, m, ctohc(ssp_ha), ctohc(sso_ha), ctohc(sst_ha), ctohc(ssn_ha), ctohc(ssa_ha) ))
 
-	# 	if not C_QUITE:
-	# 		print("[I] Завершена обработка: %s. Stat: Дубликатов ключей = %s. List: %s" % (n, db.valEDK, db.arrEDK))
+		# Детализация звонков по номерам
+		for ds in root.findall("./ds"):
+			nr = ds.attrib["n"] 		# Сетевой ресурс (номер)
+			
+			for i in ds.findall('i'):
+				db.add_detail(bn, an, nr, i.attrib)
+				# db.add_detail_commit()
+
+			if not C_QUITE:
+		 		print("[I] Завершена обработка: %s" % (nr))
+	
+	except Exception as e:
+		print("Ошибка в запросе к бд: %s" % (e,))
+		db.conn.rollback()
+		db.execute("delete from invoices where bn='%s' and an='%s'" % ( bn, an ))
 
 if __name__ == "__main__":
+
 	if len(sys.argv) <= 1:
 		print("Укажите имя файла или каталог")
 		sys.exit(-1)
+
+	if sys.argv[1].lower() == '-s':
+		print("Настраиваю базу")
+		setup()
+		sys.exit(0)
 
 	f = sys.argv[1]
 
